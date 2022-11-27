64113921a7fe39a1db70d9a94eff3430370cdddf
nikhillondhe9 <nikhillondhe9@gmail.com>
2022-11-27 17:30:48 -0500

    integrated django authentication backend

--- UserManagement/views.py
+++ UserManagement/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from .forms import RegistrationForm
 
-# Create your views here.
+
+class SignupView(View):
+    def get(self, request):
+        form = RegistrationForm()
+        return render(request, 'registration/sign_up.html', {"form": form})
+
+    def post(self, request):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            print("user registered")
+            return render(request, "registration/login.html")
+        else:
+            return render(request, 'registration/sign_up.html', {"form": form})
--- VisitorManagementSystem/settings.py
+++ VisitorManagementSystem/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'VisitorManagementSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/home'
+LOGOUT_REDIRECT_URL = '/login'
