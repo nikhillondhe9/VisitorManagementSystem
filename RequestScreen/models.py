26a9c072f0368d4ea67322d755c11ac8a84aa3f0
nikhillondhe9 <nikhillondhe9@gmail.com>
2022-11-29 17:35:09 -0500

    configured Visits request details model

--- RequestScreen/models.py
+++ RequestScreen/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Visitrequestdetails(models.Model):
+    # Field name made lowercase.
+    visitid = models.AutoField(db_column='VisitID', primary_key=True)
+    # Field name made lowercase.
+    location = models.CharField(db_column='Location', max_length=100)
+    # Field name made lowercase.
+    visitstartdatetime = models.DateTimeField(db_column='VisitStartDateTime')
+    # Field name made lowercase.
+    visitenddatetime = models.DateTimeField(db_column='VisitEndDateTime')
+    # Field name made lowercase.
+    visitstatus = models.CharField(db_column='VisitStatus', max_length=8)
+    # Field name made lowercase.
+    additionalcomments = models.TextField(
+        db_column='AdditionalComments', blank=True, null=True)
+    # Field name made lowercase.
+    visitlocationname = models.CharField(
+        db_column='VisitLocationName', max_length=100)
+    # Field name made lowercase.
+    maplink = models.CharField(
+        db_column='MapLink', max_length=100, blank=True, null=True)
+    # Field name made lowercase.
+    certifiedbyemail = models.CharField(
+        db_column='CertifiedByEmail', max_length=100)
+    # Field name made lowercase.
+    certifiedbyname = models.CharField(
+        db_column='CertifiedByName', max_length=100)
+    # Field name made lowercase.
+    createdbyemail = models.EmailField(
+        db_column='CreatedByEmail', max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'VisitRequestDetails'
